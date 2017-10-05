e43fbec790f018546274679810baffc9ab3e425e
mixelpixel <mixelpix@mac.com>
2017-10-05 16:33:24 -0400

    write a file

--- patrick/io.c
+++ patrick/io.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  FILE *p;
+  /* create a text file and put some data in it */
+  FILE *f = fopen("test.txt", "w+");
+  if (f == NULL) {
+    printf("Error opening file!\n");
+    exit(1);
+  }
+  /* add some text */
+  const char *text = "Where's my Lambda SWAG?";
+  fprintf(f, "Some text: \"%s\"\n", text);
+  /* add an integer and float */
+  int i = 1;
+  float py = 3.1415927;
+  fprintf(f, "An integer: %d\nI love Pie: %f\n", i, py);
+  /* add a single chatacters */
+  char c = 'X';
+  fprintf(f, "A character: '%c'\n", c);
+  /* close the file */
+  fclose(f);
 
-  fp = fopen("test.txt", 'r')
+  /* read in the file and display it to the console */
+
 }
