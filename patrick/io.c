08fbf20bf3be5a06bb56a7576652976e50d0b140
mixelpixel <mixelpix@mac.com>
2017-10-05 17:18:54 -0400

    how to reassign FILE variable?

--- patrick/io.c
+++ patrick/io.c
@@ -3,35 +3,36 @@
 
 int main() {
   /* create a text file and put some data in it */
-  FILE *f = fopen("test.txt", "w+");
-  if (f == NULL) {
-    printf("Error opening file!\n");
-    exit(1);
-  }
+  FILE *file = fopen("test.txt", "w+");
+  // if (file == NULL) {
+  //   printf("Error opening file!\n");
+  //   exit(1);
+  // }
   /* add some text */
   const char *text = "Where's my Lambda SWAG?";
-  fprintf(f, "Some text: \"%s\"\n", text);
+  fprintf(file, "Some text  : \"%s\"\n", text);
   /* add an integer and float */
   int i = 1;
   float py = 3.1415927;
-  fprintf(f, "An integer: %d\nI love Pie: %f\n", i, py);
+  fprintf(file, "An integer : %d\nI love Pie : %f\n", i, py);
   /* add a single chatacters */
   char c = 'X';
-  fprintf(f, "A character: '%c'\n", c);
+  fprintf(file, "A character: '%c'\n", c);
   /* close the file */
-  fclose(f);
+  fclose(file);
 
   /* read in the file and display it to the console */
-  FILE *o  = fopen("test.txt", "r");
+  /* How to reassign to "file" ??? */
+  FILE *readfile  = fopen("test.txt", "r");
   int p;
   while (1) {
-    p = fgetc(o);
-    if (feof(o)) {
+    p = fgetc(readfile);
+    if (feof(readfile)) {
       break;
     }
     printf("%c", p);
   }
-  fclose(o);
+  fclose(readfile);
 
   return 0;
 }
