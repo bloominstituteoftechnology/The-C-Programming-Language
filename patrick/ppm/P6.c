9dd080b4aa74ccc889840fd45441488b633d4727
mixelpixel <mixelpix@mac.com>
2017-10-06 11:21:58 -0400

    awesome P6 color image

--- patrick/ppm/P6.c
+++ patrick/ppm/P6.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
   const int width = 1024, height = 768;
@@ -10,9 +11,13 @@ int main(void) {
   for (row = 0; row < height; ++row) {
     for (col = 0; col < width; ++col) {
       static unsigned char color[3];
-      color[0] = col % 256;  /* red */
-      color[1] = row % 256;  /* green */
-      color[2] = (col * row) % 256;  /* blue */
+      // color[0] = col % 256;  /* red */
+      // color[1] = row % 256;  /* green */
+      // color[2] = (col * row) % 256;  /* blue */
+      color[0] = fabs(floor((row * 255 / 1024 - 35))); /* red */
+      color[1] = fabs(floor((col * 255 / 1024 - 112))); /* green */
+      color[2] = fabs(floor(((col) * 255 / 1024 - 92))); /* blue */
+
       (void) fwrite(color, 1, 3, imageFile);
     }
   }
