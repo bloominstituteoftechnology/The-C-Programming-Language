42c1df569bd6b213ebf79b97ba2f145ece0a342f
wlharvey4 <pinecone062@gmail.com>
2017-10-07 10:38:41 -0700

    templatemker.c ver 0.2_1
    produced a gradient function

--- assignment/templatemker.c
+++ assignment/templatemker.c
@@ -2,33 +2,45 @@
  * templatemker.c
  * --------------
  *
- * OCTOBER 6, 2017
- * VERSION 0.1
+ * OCTOBER 7, 2017
+ * VERSION 0.2_1
  *
  ***************************************************************************************************/
 
+#include <math.h>
+
 #include "./simple_image_machine.h"
 
-int main(int argc, char** argv) {
+typedef struct {
+  int x;
+  int y;
+  int dist;
+  int x_intersect;
+  int y_intersect;
+  int x_percent;
+  int y_percent;
+} PIXEL_INFO;
 
-  PIXEL b[1] = {GREY};
-  if ((fp = fopen("template", WRITE)) != NULL) {
-    for (int h = 0; h < HEIGHT; h++) {
-      for (int w = 0; w < WIDTH; w++) {
-        if (h < 20 || h > 747 || w < 20 || w > 1003)
-          b[0] = BLACK;
-        else
-          b[0] = WHITE;
-        if (fwrite(b, PIXEL_S, 1, fp) != 1) {
-          fprintf(stderr, "ERROR writing a pixel\n");
-          exit(EXIT_FAILURE);
-        }
-      }
+PIXEL_INFO gradient3D(int, int);
+
+int main(int argc, char** argv) {
+  for (int h = 0; h < HEIGHT; h++) {
+    for (int w = 0; w < WIDTH; w++) {
+      gradient3D(w, h);
     }
   }
-  fclose(fp);
-
-  
 }
 
+PIXEL_INFO gradient3D(int x, int y) {
+  int y_intersect = y - x; /* positive when y > x */
+  int x_intersect = x - y; /* positive when x > y */
+  int x_percent = ((float)x / WIDTH) * 100;
+  int y_percent = ((float)y / HEIGHT) * 100;
+  int dist = (int)(sqrt(((float)x * x) + ((float)y * y)));
 
+  printf("(%d, %d, %d) x%%: %d%% y%%: %d%%\n", x, y, dist, x_percent, y_percent);
+
+  return (PIXEL_INFO){
+    x, y, dist, x_intersect, y_intersect, x_percent, y_percent,
+  };
+}
