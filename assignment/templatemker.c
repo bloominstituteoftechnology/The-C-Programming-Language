926ec918ec43f464c6ec9dbda23cf3ba3f64635d
wlharvey4 <pinecone062@gmail.com>
2017-10-07 22:10:49 -0700

    templatemker.c ver 0.2_3
    Processes command-line argument -t <num>; opens template_<num> for writing

--- assignment/templatemker.c
+++ assignment/templatemker.c
@@ -3,13 +3,16 @@
  * --------------
  *
  * OCTOBER 7, 2017
- * VERSION 0.2_2
+ * VERSION 0.2_3
  *
  ***************************************************************************************************/
 
 #include <math.h>
+#include <ctype.h>
 
 #include "./simple_image_machine.h"
+
+#define MAX_NAME_S 12
 
 typedef struct {
   int x;
@@ -25,8 +28,36 @@ typedef struct {
 } PIXEL_INFO;
 
 PIXEL_INFO pixel_info(int, int);
+void all_white(PIXEL_INFO);
 
+/***************************************************************************************************
+ * MAIN                                                                                            *
+ ***************************************************************************************************/
 int main(int argc, char** argv) {
+
+  /************************************************************************
+   * template name; from -t <num>                                         *
+   ************************************************************************/
+  int ch = getopt(argc, argv, "t:");
+  if (ch == -1 || ch == '?') {
+    fprintf(stderr, "USAGE: templatemker -t <num>\n");
+    exit(EXIT_FAILURE);
+  }
+  int template_num = atoi(optarg);
+  if ( (strlen(optarg) == 1 && !isdigit(*optarg)) || template_num == 0 ) {
+    fprintf(stderr, "USAGE: templatemker -t <num>\n");
+    exit(EXIT_FAILURE);
+  }
+  char template_name[MAX_NAME_S];
+  snprintf(template_name, MAX_NAME_S, "template_%02d", template_num);
+  printf("template number %s\n", template_name);
+  if ( (fp = fopen(template_name, WRITE)) == NULL ) {
+    fprintf(stderr, "ERROR opening file %s for writing\n", template_name);
+    exit(EXIT_FAILURE);
+  } else {
+    fprintf(stderr, "SUCCESS opening file %s for writing\n", template_name);
+  }
+  
   for (int h = 0; h < HEIGHT; h++) {
     for (int w = 0; w < WIDTH; w++) {
       PIXEL_INFO info = pixel_info(w, h);
@@ -34,6 +65,9 @@ int main(int argc, char** argv) {
   }
 }
 
+/***************************************************************************************************
+ * PIXEL_INFO                                                                                      *
+ ***************************************************************************************************/
 PIXEL_INFO pixel_info(int x, int y) {
   int y_intersect = y - x; /* positive when y > x */
   int x_intersect = x - y; /* positive when x > y */
@@ -56,4 +90,8 @@ PIXEL_INFO pixel_info(int x, int y) {
       line,
       diag_gradient,
     };
+}
+
+void all_white(PIXEL_INFO pi) {
+  
 }
