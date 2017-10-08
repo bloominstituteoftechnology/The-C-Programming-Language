5388a3364603761b49fca516bdf2a05ed3582c68
wlharvey4 <pinecone062@gmail.com>
2017-10-07 20:39:33 -0700

    templatemker.c ver 0.2_2
    changed dist to z; added z percent

--- assignment/templatemker.c
+++ assignment/templatemker.c
@@ -3,7 +3,7 @@
  * --------------
  *
  * OCTOBER 7, 2017
- * VERSION 0.2_1
+ * VERSION 0.2_2
  *
  ***************************************************************************************************/
 
@@ -14,33 +14,46 @@
 typedef struct {
   int x;
   int y;
-  int dist;
+  int z;
   int x_intersect;
   int y_intersect;
   int x_percent;
   int y_percent;
+  int z_percent;
+  int line;
+  int diag_gradient;
 } PIXEL_INFO;
 
-PIXEL_INFO gradient3D(int, int);
+PIXEL_INFO pixel_info(int, int);
 
 int main(int argc, char** argv) {
   for (int h = 0; h < HEIGHT; h++) {
     for (int w = 0; w < WIDTH; w++) {
-      gradient3D(w, h);
+      PIXEL_INFO info = pixel_info(w, h);
     }
   }
 }
 
-PIXEL_INFO gradient3D(int x, int y) {
+PIXEL_INFO pixel_info(int x, int y) {
   int y_intersect = y - x; /* positive when y > x */
   int x_intersect = x - y; /* positive when x > y */
   int x_percent = ((float)x / WIDTH) * 100;
   int y_percent = ((float)y / HEIGHT) * 100;
-  int dist = (int)(sqrt(((float)x * x) + ((float)y * y)));
-
-  printf("(%d, %d, %d) x%%: %d%% y%%: %d%%\n", x, y, dist, x_percent, y_percent);
+  int z = (int)(sqrt(((float)x * x) + ((float)y * y)));
+  int z_percent = (int)(sqrt(((float)WIDTH * WIDTH) + (float)(HEIGHT * HEIGHT))) * 100;
+  int line = y_intersect >= 0 ? y : HEIGHT + x;
+  int diag_gradient = (((float)line / (WIDTH + HEIGHT)) * 100 );
 
   return (PIXEL_INFO){
-    x, y, dist, x_intersect, y_intersect, x_percent, y_percent,
-  };
+      x,
+      y,
+      z,
+      x_intersect,
+      y_intersect,
+      x_percent,
+      y_percent,
+      z_percent,
+      line,
+      diag_gradient,
+    };
 }
