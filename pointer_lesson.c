bc29f83969d1952a992cb48c66be9144f4c0460d
Sean Chen <skypemaster007@gmail.com>
2017-11-06 14:00:58 -0800

    Add comments to pointer_lesson.c

--- pointer_lesson.c
+++ pointer_lesson.c
@@ -4,21 +4,28 @@
 /***********************************************
  * This is Array Pointer Assignment Rapping 
  ***********************************************/
+
+// This function returns a pointer to a memory location that stores integers
 int* childOfMainWithAllocation() {
   // these are both 10 item integer arrays, why so obfuscated, C?
   int* integerPointer = malloc(10*sizeof(int));
-  for(int i = 0 ; i < 10 ; ++i) {
+  for (int i = 0 ; i < 10 ; ++i) {
     integerPointer[i] = i;
   }
   return integerPointer;
 }
+
 int* childOfMainWithoutAllocation() {
+  // Here, we don't use `malloc`. We just statically allocate an array to hold 10 integers.
   int integerArray[10];
-  for(int i = 0 ; i < 10 ; ++i) {
+  for (int i = 0 ; i < 10 ; ++i) {
     integerArray[i] = i;
   }
+  // The `&` operator is the inverse of the `*` operator. It outputs the actual memory address 
+  // of its operand, not the operand's value.
   return &integerArray;
 }
+
 void doThePointerAssignmentTest() {
   // what's gonna happen?
   int* integerPointer = childOfMainWithAllocation();
@@ -47,30 +54,31 @@ void doThePointerAssignmentTest() {
 /***********************************************
  * This is struct Pointer Assignment Rapping 
  ***********************************************/
+
+// A struct that represents a doubly-linked list node
+// i.e. A node that references both the node that comes before it in the list, as well as the node that comes after it
 struct radDoublyLinkedList {
   struct radDoublyLinkedList* next;
   struct radDoublyLinkedList* last;
   int value;
 } radDoublyLinkedList;
+
+// A function that allocates the necessary memory and initializes the nodes with NULL `next` and `last` fields. 
+// It should return an instance of the `radDoublyLinkedList` struct.
 struct radDoublyLinkedList* doTheStructAssignmentTest() {
-  struct radDoublyLinkedList* theList = malloc(sizeof(struct radDoublyLinkedList));
-  theList->next = NULL;
-  theList->last = NULL;
-  theList->value = 5;
-  return theList;
+  // Implement this function
 }
+
+// Adds a new doubly-linked list node to an existing node 
+// More memory will need to be allocated to the node, and its struct properties need to be initialized accordingly
 void radDoublyLinkedListAdd(struct radDoublyLinkedList* list, int value) {
-  struct radDoublyLinkedList* newItem = malloc(sizeof(radDoublyLinkedList));
-  newItem->value = value;
-  newItem->last = NULL;
-  newItem->next = list->next;
-  list = newItem;
+  // Implement this function
 }
 
 /***********************************************
  * This is Array Pointer Assignment Rapping 
  ***********************************************/
-int main(int argc, char** argv) {
+void main(int argc, char** argv) {
   doThePointerAssignmentTest();
   struct radDoublyLinkedList* list = doTheStructAssignmentTest();
   while(list->next != NULL) {
@@ -80,3 +88,4 @@ int main(int argc, char** argv) {
   }
 }
 
+// You'll need to implement the `doTheStructAssignmentTest` function and the `radDoublyLinkedListAdd` function for this code to compile.
