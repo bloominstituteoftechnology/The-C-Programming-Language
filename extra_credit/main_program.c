8ee07e098b93f4115e7a5d44ccade973a834ac67
Dylan <dylan@Dylans-MacBook.local>
2017-11-08 15:00:42 -0500

    removed typo

--- extra_credit/main_program.c
+++ extra_credit/main_program.c
@@ -19,7 +19,7 @@ void pixel_write(int i, int j, int r, int g, int b) {
 void write_pic(struct template1 temp, int x, int y) {
   fp = fopen("output.ppm", "w");
   fprintf(fp, "P6\n %i %i 255\n", 1600, 768);
-      for (int i = 0; i < 1600; i++) {
+    for (int i = 0; i < 1600; i++) {
       for(int j=  0; j < 1600; j++) {
         if (i >= x && i < (x + temp.size) && j >= y && j < (y + temp.size) && temp.stamp[i - x][j - y])
         {
