fc9a836463ef5bf5da1302d487ca2269993ad8f5
CosmonautJones <travisj_jones@hotmail.com>
2018-01-29 16:06:16 -0600

    initial

---
+++ lessons/learning-new-languages/hello/sqrt.c
@@ -0,0 +1,14 @@
+// ### Square-Root of 128934
+
+// Write a program that computes and prints the square root of 128934.
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  int num = 128934;
+
+  printf("Square root of 128934 is: %f\n", sqrt(num));
+
+  return 0;
+}
