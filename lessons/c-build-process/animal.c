5117d650444007f4d357f2b6a05816c3c8ce29b4
CosmonautJones <travisj_jones@hotmail.com>
2018-01-30 11:33:12 -0600

    finished the lessons folder

---
+++ lessons/c-build-process/animal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define GOAT_COUNT 12
+#define SHEEP_COUNT 230
+
+int main(void)
+{
+    int total = GOAT_COUNT + SHEEP_COUNT;
+    printf("I have %d animals\n", total);
+
+    return 0;
+}
