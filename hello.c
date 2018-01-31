61ed6409b53bb92b81e9763616523cee44b4f048
Sam Kim <sams404link@gmail.com>
2018-01-30 19:13:51 -0800

    hangman updated

--- hello.c
+++ hello.c
@@ -1,18 +1,30 @@
+// gcc -Wall -Wextra -o testls testls.c
+
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+
+* Main
+
+*/
 
 int main(void)
 {
-   /* Your code here */
-	char answer[8] = "hangman";
-	char guess[1];
-  printf("%s\n", answer);
-  printf("enter a letter: ");	
-	scanf("%s", guess);	
-  printf("%s\n", guess);
+//char *word = malloc(sizeof(char) * 8);
+	char  word[] = {'h','a','n','g','m','a','n'};
+  int size = sizeof(word)/sizeof(word[0]);
+  printf("%c\n", word[7]);
+  printf("%d\n", size);
 
-  for(int i = 0; i < answer.length(); i++) {
 
+  for (int i = 0; i < strlen(word); i++) {
+    printf("%c\n", word[i]);
   }
-   
-  return 0;
+  // free(word);
+ return 0;
+
 }
--- src/hangman/hangman.c
+++ src/hangman/hangman.c
@@ -15,24 +15,10 @@ char HANG_STATES[7][10 * 9] =
 
 int main(int argc, char *argv[]) {
   /* Your code here */
-	char *ans = malloc(sizeof(char) * 40);
-	ans[0] = 'h'; 
-	ans[1] = 'a'; 
-	ans[2] = 'n'; 
-	ans[3] = 'g';
-//	ans[4] = 'm'; 
-//	ans[5] = 'a'; 
-//	ans[6] = 'n';
+	char ans[] = {'/','h', 'a', 'n', 'g', 'm', 'a', 'n'};
 	char guess[1];
 	char *usedLetters = malloc(sizeof(char) * 26);
-	char *corLet = malloc(sizeof(char) * 7);
-	corLet[0] = '_'; 
-	corLet[1] = '_'; 
-	corLet[2] = '_'; 
-	corLet[3] = '_'; 
-//	corLet[4] = '_'; 
-//	corLet[5] = '_'; 
-//	corLet[6] = '_';
+	char corLet[] = {'/','_', '_','_','_','_','_','_'}; 
 	int counter = 0;
 	int arrCounter = 0;
 	int hangmanY = 0;
@@ -41,12 +27,12 @@ int main(int argc, char *argv[]) {
 	while(isGameDone != 1) {
 		// printf("%c\n", answer[0]);
 		printf("letters Used: ");
-		for (int i = 0; i < arrCounter; i++) {
+		for (int i = 1; i < arrCounter; i++) {
 			printf("%c, ", usedLetters[i]);	
 		}
 		printf("\n");
 		printf("Correct Letters so Far: ");
-		for(int i = 0; i < sizeof(ans); i++) {
+		for(int i = 1; i < sizeof(ans)/sizeof(ans[0]); i++) {
 			printf("%c ", corLet[i]);
 		};
 		printf("\n");
@@ -55,21 +41,21 @@ int main(int argc, char *argv[]) {
 
 		// printf("%s\n", guess);
 		// printf("%d",sizeof(answer));
-		for (int i = 0; i < sizeof(ans); i++) {
+		for (int i = 1; i < sizeof(ans)/sizeof(ans[0]); i++) {
 			// printf("%c\n",answer[i]);
 			if(ans[i] == guess[0]) {
 				corLet[i] = guess[0];
  				counter +=1;
 			} 
-			if ((i == sizeof(ans) - 1) && counter == 1) {
+			if ((i == sizeof(ans)/sizeof(ans[0]) - 1) && counter == 1) {
 				printf("There is %d %c.\n\n", counter, guess[0]);	
-				for(int i = 0; i < sizeof(ans); i++) {
+				for(int i = 1; i < sizeof(ans)/sizeof(ans[0]); i++) {
 					if (ans[i] != corLet[i]) {
 						break;
 					} 
-					if (i == sizeof(ans) -1) {
+					if (i == sizeof(ans)/sizeof(ans[0]) -1) {
 						printf("Correct Letters so Far: ");
-						for(int i = 0; i < sizeof(ans); i++) {
+						for(int i = 1; i < sizeof(ans)/sizeof(ans[0]); i++) {
 							printf("%c ", corLet[i]);
 						};
 						printf("\n");
@@ -79,15 +65,15 @@ int main(int argc, char *argv[]) {
 					}
 				}
 			}
-			if ((i == sizeof(ans) - 1) && counter > 1) {
+			if ((i == sizeof(ans)/sizeof(ans[0]) - 1) && counter > 1) {
 				printf("There are %d %c's.\n\n", counter, guess[0]);
-				for(int i = 0; i < sizeof(ans); i++) {
+				for(int i = 1; i < sizeof(ans)/sizeof(ans[0]); i++) {
 					if (ans[i] != corLet[i]) {
 						break;
 					}
-					if (i == sizeof(ans) -1) {
+					if (i == sizeof(ans)/sizeof(ans[0]) -1) {
 						printf("Correct Letters so Far: ");
-						for(int i = 0; i < sizeof(ans); i++) {
+						for(int i = 0; i < sizeof(ans)/sizeof(ans[0]); i++) {
 							printf("%c ", corLet[i]);
 						};
 						printf("\n");
@@ -96,9 +82,9 @@ int main(int argc, char *argv[]) {
 					}
 				}
 			}
-			if ((i == sizeof(ans) - 1) && counter == 0) {
+			if ((i == sizeof(ans)/sizeof(ans[0]) - 1) && counter == 0) {
 				printf("There is no %c.\n\n", guess[0]);
-				for (int i = 0; i < 7; i++) {
+				for (int i = 1; i < 7; i++) {
 					for (int j = hangmanY; j < hangmanY + 10 ; j++){
 						printf("%c",HANG_STATES[i][j]);
 					}
@@ -116,6 +102,6 @@ int main(int argc, char *argv[]) {
 		arrCounter += 1;
 		counter = 0;
 	}
-	free(ans), free(usedLetters), free(corLet);
+	free(usedLetters);
   return 0;
 }
